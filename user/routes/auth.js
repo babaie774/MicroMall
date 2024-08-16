848b0e2dc83a041a7d0b754f992f361777cfbacd
Alireza Babaei <babaie774@gmail.com>
2024-08-16 20:36:33 +0330

    fit: init-user-service-files

--- user/routes/auth.js
+++ user/routes/auth.js
@@ -1,15 +1,10 @@
 const express = require('express');
+const { getProfile } = require('../controller/user');
 const router = express.Router();
 
 // @desc Get user profile (JWT protected)
 // @route GET /profile
 // @access private
-router.get('/profile', async (req, res) => {
-    try {
-        res.status(200).send([]);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-})
+router.route('/getProfile').get(getProfile)
 
 module.exports = router;
