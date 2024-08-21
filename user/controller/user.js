2f8521ef3943cb241a15aaf588e7189ba30412f1
علیرضا بابایی <a.babaei@farhangian.local>
2024-08-21 15:39:23 +0330

    fix: file-names

--- user/controller/user.js
+++ user/controller/user.js
@@ -1,22 +1,6 @@
-// @desc      Get single user
-// @route     GET /api/v1/users/:id
-// @access    Private/Admin
+// @desc      Get user profile
+// @route     GET /api/v1/getProfile
+// @access    Private/User
 exports.getProfile = asyncHandler(async (req, res, next) => {
     res.status(200).json(res.advancedResults);
 });
-
-// @desc      Get single user
-// @route     GET /api/v1/users/:id
-// @access    Private/Admin
-exports.getUser = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.params.id);
-  
-    if(!user){
-      return next(new ErrorResponse(`No user with the id of ${req.params.id}`, 404));
-    }
-  
-    res.status(200).json({
-      success: true,
-      data: user
-    });
-  });
